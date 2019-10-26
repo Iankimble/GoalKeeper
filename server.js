3db6a38639467e774d047df80fa2dd8f451e2098
Ian Kimble <ian.kimble@eRT.com>
2019-10-26 10:34:46 -0400

    mongo

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 // Dependencies
 const express = require("express");
 const morgan = require("morgan");
-const app = express();
+const mongoose = require("mongoose");
+
 const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
 
 // Routes
 const routes = require("./routes/routes");
@@ -13,9 +17,19 @@ app.use(morgan("dev"));
 app.use("/", routes);
 
 // Mongo Atlas DB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Database connected"))
+  .catch(err => {
+  console.log(`db connection error : ${err.message}`)
+  process.exit()
+});
 
 // Port
-const port = 6060;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`);
 });
