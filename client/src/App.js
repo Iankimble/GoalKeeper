9a4ecc18c58f5501dd56c1158f303c783eee8e24
Ian Kimble <ian.kimble@eRT.com>
2019-12-01 11:59:51 -0500

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Loading from "./Loading";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+      loading: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  getUsers() {
+    this.setState({
+      loading: true
+    });
+    axios("https://randomuser.me/api/?results=5").then(res =>
+      this.setState({
+        users: [...this.state.users, ...res.data.results],
+        loading: false
+      })
+    );
+  }
+
+  handleSubmit(e) {
+    e.preventtDefault();
+    this.getUsers();
+    console.log("more users loaded");
+  }
+
+  componentWillMount() {
+    this.getUsers();
+  }
+
+  render() {
+    const { loading, users } = this.state;
+    return (
+      <div>
+        <h1>Sad man</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="submit" value="load users" />
+        </form>
+        <hr />
+        {!loading ? (
+          users.map(user => (
+            <div key={user.id.value}>
+              <h1 style={{ color: "green" }}>{user.name.first}</h1>
+              <h2>{user.email}</h2>
+              <hr />
+            </div>
+          ))
+        ) : (
+          <Loading message="Be skrong" />
+        )}
+      </div>
+    );
+  }
 }
-
 export default App;
